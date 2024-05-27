c0dc9a947790558c25ef18ea7acfee99a825b519
Pharez Oyelade <pharezoyelade@gmail.com>
2024-05-27 22:06:06 +0000

    finished editing new in women data

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,39 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import c2_img from './cx.jpg'
+import i2_img from './02.jpg'
+import c3_img from './c3.jpg'
+import p4_img from './product_4.png'
+import c1_img from './cy.jpg'
+import c4_img from './c4.jpg'
+
+let data_product = [
+  {
+    id:1,
+    name:"Above Knee Knot Dress",
+    image:c1_img,
+    new_price:50.00,
+    old_price:80.50,
+  },
+  {id:2,
+    name:"Long Sleeves Short Cut-Out Dress",
+    image:c2_img,
+    new_price:85.00,
+    old_price:120.50,
+  },
+  {id:3,
+    name:"Jacquard Mini Dress in Blue Floral",
+    image:c3_img,
+    new_price:60.00,
+    old_price:100.50,
+  },
+  {id:4,
+    name:"ADIVAN Short Dress",
+    image:c4_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+];
+
+export default data_product;
---
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NewLetter.css";
+
+const NewsLetter = () => {
+  return (
+    <div className="newsletter">
+      <h1>Get Exclusive Deals and New Arrivals</h1>
+      <p>Subscribe to our Newsletter</p>
+      <div>
+        <input type="email" placeholder="Enter Email" />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
+};
+
+export default NewsLetter;
